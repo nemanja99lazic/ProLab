d85bf15090b0a71acd59233baa8134b3d414ac9a
Valerijan <mv180257d@student.etf.bg.ac.rs>
2021-04-27 23:06:26 +0200

    Dodata faza 4 - Modelovanje baze podataka

---
+++ Dokumentacija/Faza4/ProLabTim (Projekat ProLab) Baza podataka.sql
@@ -0,0 +1,511 @@
+-- MySQL dump 10.13  Distrib 8.0.22, for Win64 (x86_64)
+--
+-- Host: localhost    Database: prolab_database
+-- ------------------------------------------------------
+-- Server version	8.0.21
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `administrators`
+--
+
+DROP TABLE IF EXISTS `administrators`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `administrators` (
+  `idAdministrator` int NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`idAdministrator`),
+  UNIQUE KEY `idKorisnika_UNIQUE` (`idAdministrator`),
+  CONSTRAINT `FK_administrators_idAdministrator` FOREIGN KEY (`idAdministrator`) REFERENCES `users` (`idUser`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `administrators`
+--
+
+LOCK TABLES `administrators` WRITE;
+/*!40000 ALTER TABLE `administrators` DISABLE KEYS */;
+/*!40000 ALTER TABLE `administrators` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `appointments`
+--
+
+DROP TABLE IF EXISTS `appointments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `appointments` (
+  `idAppointment` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `classroom` varchar(45) DEFAULT NULL,
+  `capacity` int NOT NULL,
+  `location` varchar(80) DEFAULT NULL,
+  `datetime` datetime NOT NULL,
+  `idLabExercise` int NOT NULL,
+  PRIMARY KEY (`idAppointment`),
+  KEY `FK_appointment_labExercise_idx` (`idLabExercise`),
+  CONSTRAINT `FK_appointments_idLabExercise` FOREIGN KEY (`idLabExercise`) REFERENCES `lab_exercises` (`idLabExercise`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `appointments`
+--
+
+LOCK TABLES `appointments` WRITE;
+/*!40000 ALTER TABLE `appointments` DISABLE KEYS */;
+/*!40000 ALTER TABLE `appointments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `attends`
+--
+
+DROP TABLE IF EXISTS `attends`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `attends` (
+  `idStudent` int NOT NULL,
+  `idSubject` int NOT NULL,
+  PRIMARY KEY (`idStudent`,`idSubject`),
+  KEY `FK_pohadja_idpredmeta_idx` (`idSubject`),
+  CONSTRAINT `FK_attends_idStudent` FOREIGN KEY (`idStudent`) REFERENCES `students` (`idStudent`) ON UPDATE CASCADE,
+  CONSTRAINT `FK_attends_idSubject` FOREIGN KEY (`idSubject`) REFERENCES `subjects` (`idSubject`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `attends`
+--
+
+LOCK TABLES `attends` WRITE;
+/*!40000 ALTER TABLE `attends` DISABLE KEYS */;
+/*!40000 ALTER TABLE `attends` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `free_agents`
+--
+
+DROP TABLE IF EXISTS `free_agents`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `free_agents` (
+  `idStudent` int NOT NULL,
+  `idAppointment` int NOT NULL,
+  `idDesiredAppointment` int NOT NULL,
+  PRIMARY KEY (`idStudent`,`idAppointment`,`idDesiredAppointment`),
+  KEY `FK_freeAgents_hasAppointment_idx` (`idAppointment`),
+  KEY `FK_freeAgents_appointments_idx` (`idDesiredAppointment`),
+  CONSTRAINT `FK_freeAgents_idAppointment` FOREIGN KEY (`idAppointment`) REFERENCES `has_appointment` (`idAppointment`) ON UPDATE CASCADE,
+  CONSTRAINT `FK_freeAgents_idDesiredAppointment` FOREIGN KEY (`idDesiredAppointment`) REFERENCES `appointments` (`idAppointment`) ON UPDATE CASCADE,
+  CONSTRAINT `FK_freeAgents_idStudent` FOREIGN KEY (`idStudent`) REFERENCES `has_appointment` (`idStudent`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `free_agents`
+--
+
+LOCK TABLES `free_agents` WRITE;
+/*!40000 ALTER TABLE `free_agents` DISABLE KEYS */;
+/*!40000 ALTER TABLE `free_agents` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `has_appointment`
+--
+
+DROP TABLE IF EXISTS `has_appointment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `has_appointment` (
+  `idAppointment` int NOT NULL,
+  `idStudent` int NOT NULL,
+  PRIMARY KEY (`idAppointment`,`idStudent`),
+  KEY `FK_hasAppointment_students_idx` (`idStudent`),
+  CONSTRAINT `FK_hasAppointment_appointments` FOREIGN KEY (`idAppointment`) REFERENCES `appointments` (`idAppointment`) ON UPDATE CASCADE,
+  CONSTRAINT `FK_hasAppointment_students` FOREIGN KEY (`idStudent`) REFERENCES `students` (`idStudent`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `has_appointment`
+--
+
+LOCK TABLES `has_appointment` WRITE;
+/*!40000 ALTER TABLE `has_appointment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `has_appointment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `lab_exercises`
+--
+
+DROP TABLE IF EXISTS `lab_exercises`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `lab_exercises` (
+  `idLabExercise` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `description` varchar(200) DEFAULT NULL,
+  `expiration` datetime NOT NULL,
+  `idSubject` int NOT NULL,
+  PRIMARY KEY (`idLabExercise`),
+  KEY `FK_subject_idx` (`idSubject`),
+  CONSTRAINT `FK_labExercices_idSubject` FOREIGN KEY (`idSubject`) REFERENCES `subjects` (`idSubject`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `lab_exercises`
+--
+
+LOCK TABLES `lab_exercises` WRITE;
+/*!40000 ALTER TABLE `lab_exercises` DISABLE KEYS */;
+/*!40000 ALTER TABLE `lab_exercises` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `new_subject_requests`
+--
+
+DROP TABLE IF EXISTS `new_subject_requests`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `new_subject_requests` (
+  `idRequest` int NOT NULL AUTO_INCREMENT,
+  `subjectName` varchar(45) NOT NULL,
+  `idTeacher` int NOT NULL,
+  PRIMARY KEY (`idRequest`),
+  KEY `FK_zahteviZaNovePredmete_idTeacher_idx` (`idTeacher`),
+  CONSTRAINT `FK_newSubjectRequests_idTeacher` FOREIGN KEY (`idTeacher`) REFERENCES `teachers` (`idTeacher`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `new_subject_requests`
+--
+
+LOCK TABLES `new_subject_requests` WRITE;
+/*!40000 ALTER TABLE `new_subject_requests` DISABLE KEYS */;
+/*!40000 ALTER TABLE `new_subject_requests` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `new_subject_requests_teaches`
+--
+
+DROP TABLE IF EXISTS `new_subject_requests_teaches`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `new_subject_requests_teaches` (
+  `idRequest` int NOT NULL,
+  `idTeacher` int NOT NULL,
+  PRIMARY KEY (`idRequest`,`idTeacher`),
+  KEY `FK_idAssociateTeacher_idx` (`idTeacher`),
+  CONSTRAINT `FK_newSubjectRequestsTeaches_idRequest` FOREIGN KEY (`idRequest`) REFERENCES `new_subject_requests` (`idRequest`) ON UPDATE CASCADE,
+  CONSTRAINT `FK_newSubjectRequestsTeaches_idTeacher` FOREIGN KEY (`idTeacher`) REFERENCES `teachers` (`idTeacher`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `new_subject_requests_teaches`
+--
+
+LOCK TABLES `new_subject_requests_teaches` WRITE;
+/*!40000 ALTER TABLE `new_subject_requests_teaches` DISABLE KEYS */;
+/*!40000 ALTER TABLE `new_subject_requests_teaches` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `projects`
+--
+
+DROP TABLE IF EXISTS `projects`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `projects` (
+  `idProject` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `minMemberNumber` varchar(45) NOT NULL,
+  `maxMemberNumber` varchar(45) NOT NULL,
+  `expirationDate` date NOT NULL,
+  `idSubject` int NOT NULL,
+  PRIMARY KEY (`idProject`),
+  KEY `FK_project_subject_idx` (`idSubject`),
+  CONSTRAINT `FK_project_idSubject` FOREIGN KEY (`idSubject`) REFERENCES `subjects` (`idSubject`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `projects`
+--
+
+LOCK TABLES `projects` WRITE;
+/*!40000 ALTER TABLE `projects` DISABLE KEYS */;
+/*!40000 ALTER TABLE `projects` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `registration_requests`
+--
+
+DROP TABLE IF EXISTS `registration_requests`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `registration_requests` (
+  `idRegistrationRequest` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `userType` varchar(1) NOT NULL,
+  PRIMARY KEY (`idRegistrationRequest`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `registration_requests`
+--
+
+LOCK TABLES `registration_requests` WRITE;
+/*!40000 ALTER TABLE `registration_requests` DISABLE KEYS */;
+/*!40000 ALTER TABLE `registration_requests` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `students`
+--
+
+DROP TABLE IF EXISTS `students`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `students` (
+  `idStudent` int NOT NULL AUTO_INCREMENT,
+  `index` varchar(9) NOT NULL,
+  PRIMARY KEY (`idStudent`),
+  UNIQUE KEY `idKorisnika_UNIQUE` (`idStudent`),
+  UNIQUE KEY `index_UNIQUE` (`index`),
+  CONSTRAINT `FK_students_idStudent` FOREIGN KEY (`idStudent`) REFERENCES `users` (`idUser`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `students`
+--
+
+LOCK TABLES `students` WRITE;
+/*!40000 ALTER TABLE `students` DISABLE KEYS */;
+/*!40000 ALTER TABLE `students` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `subject_join_requests`
+--
+
+DROP TABLE IF EXISTS `subject_join_requests`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `subject_join_requests` (
+  `idRequest` int NOT NULL AUTO_INCREMENT,
+  `idSubject` int NOT NULL,
+  `idStudent` int NOT NULL,
+  PRIMARY KEY (`idRequest`),
+  KEY `FK_idSubject_idx` (`idSubject`),
+  KEY `FK_idStudent_idx` (`idStudent`),
+  CONSTRAINT `FK_subjectJoinRequests_idStudent` FOREIGN KEY (`idStudent`) REFERENCES `students` (`idStudent`) ON UPDATE CASCADE,
+  CONSTRAINT `FK_subjectJoinRequests_idSubject` FOREIGN KEY (`idSubject`) REFERENCES `subjects` (`idSubject`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Mozda kao primary key da se stavi samo (idSubject, idStudent)?';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `subject_join_requests`
+--
+
+LOCK TABLES `subject_join_requests` WRITE;
+/*!40000 ALTER TABLE `subject_join_requests` DISABLE KEYS */;
+/*!40000 ALTER TABLE `subject_join_requests` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `subjects`
+--
+
+DROP TABLE IF EXISTS `subjects`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `subjects` (
+  `idSubject` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `code` varchar(45) NOT NULL COMMENT 'Sifra predmeta',
+  `idTeacher` int NOT NULL,
+  PRIMARY KEY (`idSubject`),
+  KEY `FK_subjects_idTeacher_idx` (`idTeacher`),
+  CONSTRAINT `FK_subjects_idTeacher` FOREIGN KEY (`idTeacher`) REFERENCES `teachers` (`idTeacher`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `subjects`
+--
+
+LOCK TABLES `subjects` WRITE;
+/*!40000 ALTER TABLE `subjects` DISABLE KEYS */;
+/*!40000 ALTER TABLE `subjects` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `teachers`
+--
+
+DROP TABLE IF EXISTS `teachers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `teachers` (
+  `idTeacher` int NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`idTeacher`),
+  CONSTRAINT `FK_teachers_idTeacher` FOREIGN KEY (`idTeacher`) REFERENCES `users` (`idUser`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `teachers`
+--
+
+LOCK TABLES `teachers` WRITE;
+/*!40000 ALTER TABLE `teachers` DISABLE KEYS */;
+/*!40000 ALTER TABLE `teachers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `teaches`
+--
+
+DROP TABLE IF EXISTS `teaches`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `teaches` (
+  `idTeacher` int NOT NULL,
+  `idSubject` int NOT NULL,
+  PRIMARY KEY (`idTeacher`,`idSubject`),
+  KEY `FK_teaches_idSubject_idx` (`idSubject`),
+  CONSTRAINT `FK_teaches_idSubject` FOREIGN KEY (`idSubject`) REFERENCES `subjects` (`idSubject`) ON UPDATE CASCADE,
+  CONSTRAINT `FK_teaches_idTeacher` FOREIGN KEY (`idTeacher`) REFERENCES `teachers` (`idTeacher`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `teaches`
+--
+
+LOCK TABLES `teaches` WRITE;
+/*!40000 ALTER TABLE `teaches` DISABLE KEYS */;
+/*!40000 ALTER TABLE `teaches` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `team_members`
+--
+
+DROP TABLE IF EXISTS `team_members`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `team_members` (
+  `idStudent` int NOT NULL,
+  `idTeam` int NOT NULL,
+  PRIMARY KEY (`idStudent`,`idTeam`),
+  KEY `FK_teams_members_idTeam_idx` (`idTeam`),
+  CONSTRAINT `FK_teamMembers_idStudent` FOREIGN KEY (`idStudent`) REFERENCES `students` (`idStudent`) ON UPDATE CASCADE,
+  CONSTRAINT `FK_teamMembers_idTeam` FOREIGN KEY (`idTeam`) REFERENCES `teams` (`idTeam`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `team_members`
+--
+
+LOCK TABLES `team_members` WRITE;
+/*!40000 ALTER TABLE `team_members` DISABLE KEYS */;
+/*!40000 ALTER TABLE `team_members` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `teams`
+--
+
+DROP TABLE IF EXISTS `teams`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `teams` (
+  `idTeam` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `locked` tinyint NOT NULL,
+  `idProject` int NOT NULL,
+  PRIMARY KEY (`idTeam`),
+  UNIQUE KEY `name_UNIQUE` (`name`),
+  KEY `gfdgdfg_idx` (`idProject`),
+  CONSTRAINT `fk_teams_idProject` FOREIGN KEY (`idProject`) REFERENCES `projects` (`idProject`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `teams`
+--
+
+LOCK TABLES `teams` WRITE;
+/*!40000 ALTER TABLE `teams` DISABLE KEYS */;
+/*!40000 ALTER TABLE `teams` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `idUser` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `forename` varchar(45) NOT NULL,
+  `surname` varchar(45) NOT NULL,
+  PRIMARY KEY (`idUser`),
+  UNIQUE KEY `idKorisnik_UNIQUE` (`idUser`),
+  UNIQUE KEY `email_UNIQUE` (`email`),
+  UNIQUE KEY `username_UNIQUE` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-04-27 22:53:51
