b3b7718009bf61e4dd0c08e46b5ebc361f0ef87b
Valerijan <mv180257d@student.etf.bg.ac.rs>
2021-05-21 00:59:34 +0200

    asdasd
    asdasd

--- Dokumentacija/ProLabProjekat/ProLab/routes/web.php
+++ Dokumentacija/ProLabProjekat/ProLab/routes/web.php
@@ -12,7 +12,7 @@ use App\Http\Controllers\HomeController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-   
+      
 
 //Route::get('/',[App\Http\Controllers\HomeController::class, 'index']);
 
