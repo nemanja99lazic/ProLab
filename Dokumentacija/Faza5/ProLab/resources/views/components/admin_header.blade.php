78a52fe8a1643255ac8860a63a450634a537e89e
zvk17 <zvk17@github.com>
2021-05-23 01:29:57 +0200

    izmena frontenda

---
+++ Dokumentacija/Faza5/ProLab/resources/views/components/admin_header.blade.php
@@ -0,0 +1,20 @@
+@component("admin_header")
+    <div class="row">
+        <div class="col-12">
+            <div class="d-flex justify-content-between">
+                <div>
+                    <button class="my-btn my-btn-tab">Zahtevi
+                        <span class="badge badge-danger" id="request-number"></span>
+                    </button>
+                    <button class="my-btn my-btn-tab">Predmeti</button>
+                    <button class="my-btn my-btn-tab">Korisnici</button>
+                </div>
+                <div class="">
+                    <button class="my-btn-tab">Početna</button>
+                    <button class="my-btn-tab">Odjava</button>
+                </div>
+            </div>
+
+        </div>
+    </div>
+@endcomponent
