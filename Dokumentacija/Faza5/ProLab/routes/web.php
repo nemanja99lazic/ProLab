20f151f04f015108753436e79ce48cf14f572cd2
zvk17 <zvk17@github.com>
2021-05-29 00:52:07 +0200

    refaktorisanje web.php

--- Dokumentacija/Faza5/ProLab/routes/web.php
+++ Dokumentacija/Faza5/ProLab/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\StudentController;
+use App\Http\Controllers\TeacherController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\GuestController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -22,75 +25,75 @@ use Illuminate\Support\Facades\Route;
 //Funkcionalnosti :
 // Login, Logout, Register, Index
 //
-Route::get("/testAdmin", [\App\Http\Controllers\HomeController::class, "testAdmin"])->name("testadmin");
+Route::get("/testAdmin", [HomeController::class, "testAdmin"])->name("testadmin");
 
 //Guest
 
 
 
-Route::get('/',[App\Http\Controllers\GuestController::class, 'loginGet'])->name('guest.login.get');  //DEFAULT ruta
-Route::post('/',[App\Http\Controllers\GuestController::class, 'loginPost'])->name('guest.login.post');
+Route::get('/',[GuestController::class, 'loginGet'])->name('guest.login.get');  //DEFAULT ruta
+Route::post('/',[GuestController::class, 'loginPost'])->name('guest.login.post');
 
 
-Route::get('/register',[App\Http\Controllers\GuestController::class, 'registerGet'])->name('guest.register.get');
-Route::post('/register',[App\Http\Controllers\GuestController::class, 'registerPost'])->name('guest.register.post');
-Route::get('/register_info',[App\Http\Controllers\GuestController::class, 'registerInfo'])->name('guest.registerinfo');
+Route::get('/register',[GuestController::class, 'registerGet'])->name('guest.register.get');
+Route::post('/register',[GuestController::class, 'registerPost'])->name('guest.register.post');
+Route::get('/register_info',[GuestController::class, 'registerInfo'])->name('guest.registerinfo');
 
 //Student
 
-Route::get('/student',[App\Http\Controllers\StudentController::class, 'index'])->name('student.index');
-Route::get('/student/logout',[App\Http\Controllers\StudentController::class, 'logout'])->name('student.logout');
-Route::get('/student/subject/enroll', [App\Http\Controllers\StudentController::class, 'showAllSubjectsList'])->name('student.showAllSubjectsList');
-Route::post('/student/subject/enroll', [App\Http\Controllers\StudentController::class, 'sendJoinRequest'])->name('student.sendJoinRequest');
+Route::get('/student',[StudentController::class, 'index'])->name('student.index');
+Route::get('/student/logout',[StudentController::class, 'logout'])->name('student.logout');
+Route::get('/student/subject/enroll', [StudentController::class, 'showAllSubjectsList'])->name('student.showAllSubjectsList');
+Route::post('/student/subject/enroll', [StudentController::class, 'sendJoinRequest'])->name('student.sendJoinRequest');
 
 // IZBRISI OVO
-Route::get('/student/test', [App\Http\Controllers\StudentController::class, 'test'])->name('student.test');
+Route::get('/student/test', [StudentController::class, 'test'])->name('student.test');
 
-Route::get('/student/chosen',[App\Http\Controllers\StudentController::class, 'chosen'])->name('student.chosen');
+Route::get('/student/chosen',[StudentController::class, 'chosen'])->name('student.chosen');
 // KOMENTARISI RED ISPOD, OVO SRETEN RADI
-Route::get('/student/subject/{code}/index',[App\Http\Controllers\StudentController::class, 'index'])->name('student.subject.index');
+Route::get('/student/subject/{code}/index',[StudentController::class, 'index'])->name('student.subject.index');
 
-Route::get('/student/subject/{code}/lab',[App\Http\Controllers\StudentController::class, 'lab'])->name('student.subject.lab');
-Route::get('/student/subject/{code}/lab/{idLab}/join',[App\Http\Controllers\StudentController::class, 'showAppointments'])->name('student.subject.lab.idlab.join.get');
-Route::post('/student/subject/{code}/lab/{idLab}/join',[App\Http\Controllers\StudentController::class, 'joinAppointment'])->name('student.subject.lab.idlab.join.post');
-Route::post('/student/subject/{code}/lab/{idLab}/leave',[App\Http\Controllers\StudentController::class, 'leaveAppointment'])->name('student.subject.lab.idlab.leave');
-Route::get('/student/subject/{code}/lab/{idLab}/swap',[App\Http\Controllers\StudentController::class, 'showPossibleSwaps'])->name('student.subject.code.lab.idlab.swap.get');
-Route::post('/student/subject/{code}/lab/{idLab}/swap',[App\Http\Controllers\StudentController::class, 'performSwap'])->name('student.subject.code.lab.idlab.swap.post');
+Route::get('/student/subject/{code}/lab',[StudentController::class, 'lab'])->name('student.subject.lab');
+Route::get('/student/subject/{code}/lab/{idLab}/join',[StudentController::class, 'showAppointments'])->name('student.subject.lab.idlab.join.get');
+Route::post('/student/subject/{code}/lab/{idLab}/join',[StudentController::class, 'joinAppointment'])->name('student.subject.lab.idlab.join.post');
+Route::post('/student/subject/{code}/lab/{idLab}/leave',[StudentController::class, 'leaveAppointment'])->name('student.subject.lab.idlab.leave');
+Route::get('/student/subject/{code}/lab/{idLab}/swap',[StudentController::class, 'showPossibleSwaps'])->name('student.subject.code.lab.idlab.swap.get');
+Route::post('/student/subject/{code}/lab/{idLab}/swap',[StudentController::class, 'performSwap'])->name('student.subject.code.lab.idlab.swap.post');
 
 //Teacher
 
-Route::get('/teacher',[App\Http\Controllers\TeacherController::class, 'index'])->name('teacher.index');
-Route::get('/teacher/logout',[App\Http\Controllers\TeacherController::class, 'logout'])->name('teacher.logout');
-Route::get('/teacher/subject/list',[App\Http\Controllers\HomeController::class, 'getSubjects'])->name('teacher.subject.list');
-Route::get('/teacher/subject/{idSubject}/index',[App\Http\Controllers\HomeController::class, 'getSubject'])->name('teacher.subject.page');
-Route::get('/teacher/addSubject',[App\Http\Controllers\TeacherController::class, 'addSubjectGet'])->name('teacher.addsubject.get');
-Route::post('/teacher/addSubject',[App\Http\Controllers\TeacherController::class, 'addSubjectPost'])->name('teacher.addsubject.post');
-Route::get  ('/teacher/addSubject/info',[App\Http\Controllers\TeacherController::class, 'addSubjectInfo'])->name('teacher.addsubject.info');
+Route::get('/teacher',[TeacherController::class, 'index'])->name('teacher.index');
+Route::get('/teacher/logout',[eacherController::class, 'logout'])->name('teacher.logout');
+Route::get('/teacher/subject/list',[HomeController::class, 'getSubjects'])->name('teacher.subject.list');
+Route::get('/teacher/subject/{idSubject}/index',[HomeController::class, 'getSubject'])->name('teacher.subject.page');
+Route::get('/teacher/addSubject',[TeacherController::class, 'addSubjectGet'])->name('teacher.addsubject.get');
+Route::post('/teacher/addSubject',[TeacherController::class, 'addSubjectPost'])->name('teacher.addsubject.post');
+Route::get  ('/teacher/addSubject/info',[TeacherController::class, 'addSubjectInfo'])->name('teacher.addsubject.info');
 
-Route::get('/teacher/subject/request/list', [App\Http\Controllers\TeacherController::class, 'showRequestsList'])->name('teacher.showRequestsList');
-Route::post('/teacher/subject/request/list/accept', [App\Http\Controllers\TeacherController::class, 'acceptRequest'])->name('teacher.acceptRequest');
-Route::post('/teacher/subject/request/list/reject', [App\Http\Controllers\TeacherController::class, 'rejectRequest'])->name('teacher.rejectRequest');
+Route::get('/teacher/subject/request/list', [TeacherController::class, 'showRequestsList'])->name('teacher.showRequestsList');
+Route::post('/teacher/subject/request/list/accept', [TeacherController::class, 'acceptRequest'])->name('teacher.acceptRequest');
+Route::post('/teacher/subject/request/list/reject', [TeacherController::class, 'rejectRequest'])->name('teacher.rejectRequest');
 
-Route::get('/teacher/subject/{code}/project', [App\Http\Controllers\TeacherController::class, 'showProjects'])->name('teacher.showProjects');
-Route::get('/teacher/subject/{code}/project/define', [App\Http\Controllers\TeacherController::class, 'showProjectForm'])->name('teacher.showProjectForm');
-Route::post('/teacher/subject/{code}/project/removeProject', [App\Http\Controllers\TeacherController::class, 'removeProject'])->name('teacher.removeProject');
+Route::get('/teacher/subject/{code}/project', [TeacherController::class, 'showProjects'])->name('teacher.showProjects');
+Route::get('/teacher/subject/{code}/project/define', [TeacherController::class, 'showProjectForm'])->name('teacher.showProjectForm');
+Route::post('/teacher/subject/{code}/project/removeProject', [TeacherController::class, 'removeProject'])->name('teacher.removeProject');
 
 //Admin
 
-Route::get('/admin',[App\Http\Controllers\AdminController::class, 'registerRequests'])->name('admin.index');
+Route::get('/admin',[AdminController::class, 'registerRequests'])->name('admin.index');
 
-Route::get('/admin/requests/register',[App\Http\Controllers\AdminController::class, 'registerRequests'])->name('admin.register.requests');
-Route::post('/admin/requests/register/addUser',[App\Http\Controllers\AdminController::class, 'addUser'])->name('admin.addUser');
-Route::post('/admin/requests/register/delete',[App\Http\Controllers\AdminController::class, 'deleteRegisterRequest'])->name('admin.deleteRequest.register');
+Route::get('/admin/requests/register',[AdminController::class, 'registerRequests'])->name('admin.register.requests');
+Route::post('/admin/requests/register/addUser',[AdminController::class, 'addUser'])->name('admin.addUser');
+Route::post('/admin/requests/register/delete',[AdminController::class, 'deleteRegisterRequest'])->name('admin.deleteRequest.register');
 
-Route::get('/admin/requests/newSubjects',[App\Http\Controllers\AdminController::class, 'newSubjectRequests'])->name('admin.newSubject.requests');
-Route::post('/admin/requests/newSubjects/addSubject', [\App\Http\Controllers\AdminController::class, 'addSubject'])->name('admin.addSubject');
-Route::post('/admin/requests/newSubjects/delete', [\App\Http\Controllers\AdminController::class, 'deleteSubjectRequest'])->name('admin.deleteRequest.subject');
+Route::get('/admin/requests/newSubjects',[AdminController::class, 'newSubjectRequests'])->name('admin.newSubject.requests');
+Route::post('/admin/requests/newSubjects/addSubject', [AdminController::class, 'addSubject'])->name('admin.addSubject');
+Route::post('/admin/requests/newSubjects/delete', [AdminController::class, 'deleteSubjectRequest'])->name('admin.deleteRequest.subject');
 
-Route::get('/admin/subjects/list', [\App\Http\Controllers\AdminController::class, 'subjectList'])->name('admin.subjects.list');
-Route::post('/admin/requests/subjects/{id}/delete', [\App\Http\Controllers\AdminController::class, 'deleteSubject'])->name('admin.delete.subject');
+Route::get('/admin/subjects/list', [AdminController::class, 'subjectList'])->name('admin.subjects.list');
+Route::post('/admin/requests/subjects/{id}/delete', [AdminController::class, 'deleteSubject'])->name('admin.delete.subject');
 
-Route::get('/admin/logout',[App\Http\Controllers\AdminController::class, 'logout'])->name('admin.logout');
-//Route::post('/admin/addUser',[App\Http\Controllers\AdminController::class, 'addUser'])->name('admin.adduser');
-//Route::post('/admin/deleteRequest',[App\Http\Controllers\AdminController::class, 'deleteRequest'])->name('admin.deleterequest');
+Route::get('/admin/logout',[AdminController::class, 'logout'])->name('admin.logout');
+//Route::post('/admin/addUser',[AdminController::class, 'addUser'])->name('admin.adduser');
+//Route::post('/admin/deleteRequest',[AdminController::class, 'deleteRequest'])->name('admin.deleterequest');
 
