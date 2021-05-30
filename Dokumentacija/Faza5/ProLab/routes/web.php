b741f1b0b3053c694640c575efffcc8e16c26531
zvk17 <zvk17@github.com>
2021-05-30 15:45:52 +0200

    manje izmene u repo

--- Dokumentacija/Faza5/ProLab/routes/web.php
+++ Dokumentacija/Faza5/ProLab/routes/web.php
@@ -51,7 +51,7 @@ Route::post('/student/subject/enroll', [StudentController::class, 'sendJoinReque
 Route::get('/student/test', [StudentController::class, 'test'])->name('student.test');
 
 Route::get('/student/chosen',[StudentController::class, 'chosen'])->name('student.chosen');
-// KOMENTARISI RED ISPOD, OVO SRETEN RADI
+// TODO KOMENTARISI RED ISPOD, OVO SRETEN RADI
 Route::get('/student/subject/{code}/index',[StudentController::class, 'index'])->name('student.subject.index');
 
 Route::get('/student/subject/{code}/lab',[StudentController::class, 'lab'])->name('student.subject.lab');
